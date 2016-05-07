73f2babc99117522ba7ce3ee6645d1cdfd32daa4
Rachit Garg <rox.rachit@gmail.com>
2016-05-07 22:29:15 +0530

    Added chefs api to the ui + chefs can be seen after login + minor ui changes

--- app/modules/dashboard/dashboard.js
+++ app/modules/dashboard/dashboard.js
@@ -2,12 +2,22 @@
 	'use strict'
 
 	angular.module('chefSocial')
-	.controller('dashboard', ['$rootScope', '$scope', 'UserService', '$localStorage', '$state', 'ChefService', '$auth',
-		function ($rootScope, $scope, UserService, $localStorage, $state, ChefService, $auth){
+	.controller('dashboard', ['$rootScope', '$scope', '$localStorage', '$state', 'ChefService', '$auth',
+		function ($rootScope, $scope, $localStorage, $state, ChefService, $auth){
 
 			var init = function(){
-
+				ChefService.fetchChefs()
+					.then(
+						function (response){
+							// Success
+							$scope.chefs = response.chefs;
+							console.log("success....");
+						}, 
+						function (response){
+							// Error
+						});
 			};
+			init();
 
 		}]);
 })(window.angular);
